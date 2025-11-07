300ee0001edc448f64ed9cd1dc661d1fcee6e542
Alexandre Machado Rosa Filho <alxmrf04@gmail.com>
2025-11-07 03:41:17 -0300

    feat: adicionado a funcao de expandir para as caixinhas de consultor

--- src/app/shared/components/consultor-box/consultor-box.ts
+++ src/app/shared/components/consultor-box/consultor-box.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { Consultor } from '../../models/ConsultorModel';
+import { CommonModule, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-consultor-box',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,NgStyle],
   templateUrl: './consultor-box.html',
   styleUrl: './consultor-box.scss',
 })
 export class ConsultorBox {
   @Input() consultor!: Consultor;
+  isExpanded: boolean = false;
+  ngOnInit() {
+  }
+
+  expandedSize = {
+    "width": "500px",
+    "height":'500px',
+  };
+
+  setExpanded() {
+    this.isExpanded = !this.isExpanded;
+    console.log(this.isExpanded);
+  }
 }
